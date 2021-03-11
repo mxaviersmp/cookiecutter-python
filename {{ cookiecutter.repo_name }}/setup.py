f5702241214e4b9482faa5a34b6f17be892b5f31
flych3r <xavier_sampaio@alu.ufc.br>
2021-03-11 20:43:17 -0300

    added cd

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,24 +1,19 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     LONG_DESCRIPTION = f.read()
 
 with open('requirements.txt') as f:
     DEPENDENCIES = f.readlines()
-
-
-if '{{ cookiecutter.license }}' == 'MIT':
-    LICENSE = 'MIT License'
-elif '{{ cookiecutter.license }}' == 'BSD-3':
-    LICENSE = 'BSD License'
-elif '{{ cookiecutter.license }}' == 'Mozilla 2.0':
-    LICENSE = 'Mozilla Public License 2.0 (MPL 2.0)'
-else:
-    LICENSE = None
-
+{% if cookiecutter.license == 'MIT' %}
+LICENSE = 'MIT License'
+{% elif cookiecutter.license == 'BSD-3' %}
+LICENSE = 'BSD License'
+{% elif cookiecutter.license == 'Mozilla 2.0' %}
+LICENSE = 'Mozilla Public License 2.0 (MPL 2.0)'
+{% else %}
+LICENSE = None
+{% endif %}
 CLASSIFIERS = [
     'Development Status :: 1 - Planning',
     'Programming Language :: Python :: 3',
@@ -38,7 +33,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     url='{{ cookiecutter.repo_name }}',
-    packages=find_packages('{{ cookiecutter.project_src }}', exclude=['tests*']),
+    packages=find_packages(exclude=['*.tests']),
     classifiers=CLASSIFIERS,
     install_requires=DEPENDENCIES,
     include_package_data=True
