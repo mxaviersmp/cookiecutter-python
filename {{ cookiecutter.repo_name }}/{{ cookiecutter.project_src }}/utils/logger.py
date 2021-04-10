777cbe2c51d8c694da8ccdcb026d6eef37c6f09f
flych3r <matheus.sampaio011@gmail.com>
2021-04-10 15:16:25 -0300

    :loud_sound: Changed logger to loguru

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_src }}/utils/logger.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_src }}/utils/logger.py
@@ -1,34 +1,21 @@
-import logging
+from loguru import logger
+import sys
 import os
 
-try:
-    from rich.logging import RichHandler
-    RICH = True
-except ImportError:
-    RICH = False
-
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 LOG_FILE = os.getenv('LOG_FILE', None)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(LOG_LEVEL)
+logger.remove()
 
-if RICH:
-    shell_handler = RichHandler()
-    fmt_shell = '%(message)s'
-else:
-    shell_handler = logging.StreamHandler()
-    fmt_shell = '%(levelname)s %(asctime)s %(message)s'
+format = \
+    '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>' \
+    ' | ' \
+    '<level>{level: <8}</level>' \
+    ' | ' \
+    '<cyan>{file}</cyan>:<cyan>{line}</cyan> @<cyan>{function}</cyan>' \
+    ' - ' \
+    '<level>{message}</level>'
 
-shell_handler.setLevel(LOG_LEVEL)
-shell_formatter = logging.Formatter(fmt_shell)
-shell_handler.setFormatter(shell_formatter)
-logger.addHandler(shell_handler)
-
+logger.add(sys.stdout, format=format, level=LOG_LEVEL)
 if LOG_FILE:
-    file_handler = logging.FileHandler(LOG_FILE)
-    file_handler.setLevel(LOG_LEVEL)
-    fmt_file = '%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
-    file_formatter = logging.Formatter(fmt_file)
-    file_handler.setFormatter(file_formatter)
-    logger.addHandler(file_handler)
+    logger.add(LOG_FILE, format=format, level=LOG_LEVEL)
